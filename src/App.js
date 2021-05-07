5a532f75a7e313984a4ec910ef4439f69e3dcaff
Jonathan Dawson <dawson.jon@hotmail.com>
2021-05-07 18:56:55 +0100

    made simpleSlider a component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
 import React from 'react';
-import Slider from 'react-slick';
-import './styling/slider.css';
-
-function SimpleSlider() {
-    const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
-  return (
-    <div class="container">
-        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-				<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      <h2> Single Item</h2>
-      <Slider {...settings}>
-        <div>
-          <img src='http://placekitten.com/g/400/202' />
-        </div>
-        <div>
-          <img src='http://placekitten.com/g/400/201' />
-        </div>
-        <div>
-          <img src='http://placekitten.com/g/400/203' />
-        </div>
-      </Slider>
-    </div>
-   );
- };
+import SimpleSlider from './components/simpleSlider';
 
 function App() {
   return (
