60f09ac10d5026226ec10e37dd9b38a944615aaf
Jonathan Dawson <dawson.jon@hotmail.com>
2021-05-07 17:29:07 +0100

    blank play area setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import GoalList from './components/GoalList';
-import './App.css';
+import './styling/home.css';
 
 const App = () => {
-  const courseGoals = [
-    {id: 'cg1', text: 'Finish the Course'},
-    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
-    {id: 'cg3', text: 'Help other students in the course Q&A'},
-  ];
 
-  // JSX
   return (
-    <div className="course-goals">
-      <h2>Course Goals</h2>
-      <GoalList goals={courseGoals} />
+    <div className="header">
+      <h2> React Play Area </h2>
     </div>
   )
 }
-
-// const App is a component
-// props means you can pass data from component A (App) to component B (GoalList)
-// every component in react must return JSX 
 
 export default App;
