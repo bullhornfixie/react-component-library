c7dd60d6dc170564ad4d04a193479ab217a48bf3
Jon Dawson <dawson.jon@hotmail.com>
2021-06-02 16:40:42 +0100

    hover button

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 import { Flex, Box, Text } from 'rebass'
 import { font, colors, sizes } from 'styles/variables'
+import  Menu from './hover-button'
 
 const Header = () => (
   <Box sx={{ position: 'relative' }}>
     <Flex py="30px" alignItems="center" justifyContent="space-around" bg={colors.black}>
+      <Button />
       <Text
         fontFamily={font.family.mono}
         color={colors.white}
