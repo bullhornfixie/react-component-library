11718421814043fbc460c0f241cd7f64f043e6f6
Jon Dawson <dawson.jon@hotmail.com>
2021-07-22 17:07:14 +0100

    horizontal line

---
+++ src/components/horizontalLine.js
@@ -0,0 +1,11 @@
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: `${colors.pvBlue}`,
+            height: 1
+        }}
+    />
+);
+
+<ColoredLine />
