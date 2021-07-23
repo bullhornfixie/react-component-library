7728dea9a6852132577c58bda3604234e6b0bf7d
Jon Dawson <dawson.jon@hotmail.com>
2021-07-23 14:12:48 +0100

    updated circle logo

--- src/components/circleLogo.js
+++ src/components/circleLogo.js
@@ -1,8 +1,11 @@
-<Image 
-    src='/logo.svg'
-    width="300px"
-    height="300px"
-    sx={{borderRadius: "1000px"}}
-    bg={colors.white}
-    padding={spacing.base}
-/>
+const circleLogo = logos.map(logo => (
+    <Image 
+      src={logo.fields.file.url} 
+      width={["100px", "300px"]}
+      height={["100px", "300px"]}
+      sx={{borderRadius: "1000px", objectFit: "contain", backgroundSize: "contain"   }}
+      bg={colors.white}
+      margin="30px"
+    />
+  )
+)
