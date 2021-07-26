2b91cf89898094afbd7c0049c9f9b1ac07e7d7d3
Jon Dawson <dawson.jon@hotmail.com>
2021-07-26 09:20:10 +0100

    flexBox with linear gradient bg:

---
+++ src/components/styling/linearGradient.js
@@ -0,0 +1,12 @@
+
+
+<Flex
+  flexDirection='column'
+  width={1}
+  minHeight='100vh'
+  bg={backgroundColor}
+  px={`${spacing.md}px`}
+  sx={{
+    backgroundImage: 'linear-gradient(270deg, #606C7D 1.67%, #0F2340 54.17%);'
+  }}>
+</Flex>
