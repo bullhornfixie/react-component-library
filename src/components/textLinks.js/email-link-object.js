8a4a57a908fb13d7643e209a9c29be90d7099223
Jon Dawson <dawson.jon@hotmail.com>
2021-07-26 17:26:57 +0100

    cleaned up code in email link

--- src/components/textLinks.js/email-link-object.js
+++ src/components/textLinks.js/email-link-object.js
@@ -1,16 +1,11 @@
 import Link from 'next/link'
 import { FaLinkedin } from 'react-icons/fa'
 
-<MemberText mt={spacing.sm} fontSize={sizes.sm}>
-  {member.fields.title}
-</MemberText>
-<MemberText mt={spacing.sm} fontSize={sizes.sm}>
+<Flex>
   <Link href={"mailto:" + `${member.fields.email}`}>
     <a>{member.fields.email}</a>
   </Link>
-</MemberText>
-<MemberText mt={spacing.sm} fontSize={sizes.sm}>
   <Link href={member.fields.linkedIn}>
     <a><FaLinkedin fontSize={30} /></a>
   </Link>
-</MemberText>
+ </Flex>
