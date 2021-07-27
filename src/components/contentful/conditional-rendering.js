d47e08ee991bffeb99f8ff52ce1a5a8c71dcd017
Jon Dawson <dawson.jon@hotmail.com>
2021-07-27 09:46:11 +0100

    conditional rendering

---
+++ src/components/contentful/conditional-rendering.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Copy from '../data/copy'
+
+// Example1
+// {portraitWording? <div text={Copy.formText2} /> : <div text={Copy.formText1} />}
+
+// Example2 
+// {!isEmpty(teamPhotos) && teamPhotos.map…
