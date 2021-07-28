04ffaaa088ab2f5ed8863ba812a4ce066c5639f5
Jon Dawson <dawson.jon@hotmail.com>
2021-07-28 14:12:48 +0100

    contentful API call

---
+++ src/components/contentful/apiCall.js
@@ -0,0 +1,22 @@
+import { contentfulClient } from '@services/contentful'
+
+// const [loaded, setLoaded] = useState(false)
+//   const [content, setContent] = useState([])
+
+//   const fetchData = async () => {
+//     const entriesObj = {
+//       content_type: 'risk',
+//       'sys.id': '4OGzRpb3Shy935zKq4lYMY',
+//       include: 1,
+//     }
+  
+//     const response = await contentfulClient.getEntries(entriesObj)
+//     if (response) {
+//       setLoaded(true)
+//       setContent(response.items[0].fields)
+//     }
+//   }
+
+//   useEffect(() => {
+//     fetchData()
+//   }, [])
